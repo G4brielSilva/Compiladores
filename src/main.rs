aaef45a0eaeabd8dd531c9cab9734ffef4aac1a5
G4brielSilva <gabrielsilvacarvalho@id.uff.br>
2024-04-18 11:45:53 -0300

    feat: adicionando leitura de arquivo, se separação por '\n' e ' '

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,18 @@ mod node;
 use enum_token::Token;
 use node::Node;
 use std::collections::LinkedList;
+use std::fs::File;
+use std::io::prelude::*;
 
-fn main() {
+fn separate_string(contents: &String) -> Vec<&str> {
+    let delimiters = vec!['\n', ' '];
+    let subs = contents.split(&delimiters[..])
+                    .filter(|s| !s.is_empty())
+                    .collect::<Vec<&str>>();
+    subs
+}
+
+fn main() -> std::io::Result<()> {
     let mut list: LinkedList<Node> = LinkedList::new();
     
     for value in 0..10 {
@@ -18,4 +28,12 @@ fn main() {
     for elem in &list {
         println!("{} {}", elem.value, "Token Struct");
     }
+
+    let mut file = File::open("./test.jaca")?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+
+    let subs = separate_string(&contents);
+    println!("{:?}", subs);
+    Ok(())
 }
